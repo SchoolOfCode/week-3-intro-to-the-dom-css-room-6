9be67514381c67b1ce6c07ce52fb1faca0da4ea0
“graceoreillygit config --global user.email “graceoreilly111@gmail.com <yourname@example.comf>
2024-12-09 12:46:16 +0000

    Ticket 1a to 1d complete

---
+++ main.js
@@ -0,0 +1,20 @@
+// console.log("Linked!");
+
+let h1 = document.getElementById("title");
+// console.log(h1)
+
+let x = document.getElementsByClassName("content");
+// console.log(x);
+
+let tagName = document.getElementsByTagName("li");
+// console.log(tagName);
+
+let queryVar = document.querySelector(".highlight");
+console.log(queryVar);
+
+// // Ticket 1d: querySelector()
+// Instructions:
+// Use querySelector() to select the first element with the class "highlight".
+// Log the selected element to the console.
+// Expected Outcome:
+// The console should display a single <p> element with the content "This is another paragraph with the class 'content' and 'highlight'."
